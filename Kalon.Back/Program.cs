c957d792ee4a65b23a3333176c5da51da7597ddb
nicovig <nicolas.vigou@gmail.com>
2026-04-08 15:43:27 +0200

    database Migrate on each time

--- Kalon.Back/Program.cs
+++ Kalon.Back/Program.cs
@@ -66,7 +66,7 @@ if (app.Environment.IsDevelopment())
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    dbContext.Database.EnsureCreated();
+    dbContext.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
